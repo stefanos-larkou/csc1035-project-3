3bff780b7370a08c4d112cde5bd6920c7846b5dd
c0034805 <c0034805@newcastle.ac.uk>
2021-05-04 22:42:16 +0100

    make all variables final

--- src/uk/ac/ncl/Constants.java
+++ src/uk/ac/ncl/Constants.java
@@ -11,11 +11,11 @@ import java.awt.*;
 
 public class Constants {
     public static final String EMPTY_STRING = "";
-    public static int BOARD_SIZE = 8;
-    public static CellStatus PLAYERS_CELL_STATUS = CellStatus.DARK;
-    public static CellStatus OPPONENTS_CELL_STATUS = CellStatus.LIGHT;
-    public static Color PLAYERS_COLOUR = Color.BLACK;
-    public static Color OPPONENTS_COLOUR = Color.WHITE;
-    public static Color BOARD_COLOUR = new Color(820000);
-    public static int DELAY_TIME = 400;
+    public static final int BOARD_SIZE = 8;
+    public static final CellStatus PLAYERS_CELL_STATUS = CellStatus.DARK;
+    public static final CellStatus OPPONENTS_CELL_STATUS = CellStatus.LIGHT;
+    public static final Color PLAYERS_COLOUR = Color.BLACK;
+    public static final Color OPPONENTS_COLOUR = Color.WHITE;
+    public static final Color BOARD_COLOUR = new Color(820000);
+    public static final int DELAY_TIME = 400;
 }
